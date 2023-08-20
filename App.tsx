88ee008ab662487e850ca19cde57b5b9f9f668e4
Mashwishi <Mashwishiii@gmail.com>
2023-08-20 14:55:59 +0800

    49.0.3

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-/// <reference types="nativewind/types" />
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import AppNavigator from './src/navigation';
 
-export default function App() {
-  
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-
+function App(): JSX.Element {
+  return <AppNavigator/>;
 }
+
+export default App;
---
+++ src/navigation/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+const AppNavigator = () => {
+
+  return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className='text-gray-500 font-bold'>Your App is working!</Text>
+        </View>
+  );
+};
+
+export default AppNavigator;
